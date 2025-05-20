5c26da35e66a6139db00fcb3a6d10b86e6ec8788
Dhruvil <dhruvilprajapati2003@gmail.com>
2025-05-20 18:02:55 +0530

    removed extra ice servers thus improving latency

--- src/config/iceconfig.jsx
+++ src/config/iceconfig.jsx
@@ -4,7 +4,6 @@ const iceconfig = {
             urls: [
                 'stun:stun.relay.metered.ca:80', 
                 'stun:stun1.l.google.com:19302', 
-                'stun:stun2.l.google.com:19302',
             ],
         },
         {
@@ -12,16 +11,16 @@ const iceconfig = {
             username: import.meta.env.VITE_ICE_USERNAME,
             credential: import.meta.env.VITE_ICE_PASSWORD,
         },
-        {
-            urls: import.meta.env.VITE_ICE_TURN_SERVER_2,
-            username: import.meta.env.VITE_ICE_USERNAME,
-            credential: import.meta.env.VITE_ICE_PASSWORD,
-        },
-        {
-            urls: import.meta.env.VITE_ICE_TURN_SERVER_3,
-            username: import.meta.env.VITE_ICE_USERNAME,
-            credential: import.meta.env.VITE_ICE_PASSWORD,
-        },
+        // {
+        //     urls: import.meta.env.VITE_ICE_TURN_SERVER_2,
+        //     username: import.meta.env.VITE_ICE_USERNAME,
+        //     credential: import.meta.env.VITE_ICE_PASSWORD,
+        // },
+        // {
+        //     urls: import.meta.env.VITE_ICE_TURN_SERVER_3,
+        //     username: import.meta.env.VITE_ICE_USERNAME,
+        //     credential: import.meta.env.VITE_ICE_PASSWORD,
+        // },
         {
             urls: import.meta.env.VITE_ICE_TURN_SERVER_4,
             username: import.meta.env.VITE_ICE_USERNAME,
@@ -29,6 +28,6 @@ const iceconfig = {
         },
     ],
     // To prefetch ice Candidate before setting local description range(0-255) more better but use more resource
-    iceCandidatePoolSize: 10,
+    iceCandidatePoolSize: 4,
 };
 export default iceconfig;
